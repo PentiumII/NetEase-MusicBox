4a570e0af51a010738cb33d01d8571839a02fc65
vellow <seaplor@gmail.com>
2014-07-18 00:25:27 +0800

    bug fix

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'NetEase-MusicBox',
-    version = '0.1.0.8',
+    version = '0.1.0.9',
     packages = find_packages(),
 
     include_package_data = True,
