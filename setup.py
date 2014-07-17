14589bfa7da657d176691934742ae24dc327b248
vellow <seaplor@gmail.com>
2014-07-17 21:14:59 +0800

    v-0.1.0.7 release up

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'NetEase-MusicBox',
-    version = '0.1.0.6',
+    version = '0.1.0.7',
     packages = find_packages(),
 
     include_package_data = True,
